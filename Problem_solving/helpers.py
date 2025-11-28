d7d77045b11d77ea08a31079ee72c72e7ae0e56b
Teresa Klatzer <teresa.klatzer@gmail.com>
2025-11-28 15:43:34 +0000

    removed some commented code

--- Problem_solving/helpers.py
+++ Problem_solving/helpers.py
@@ -5,22 +5,14 @@ import scipy
 
 def s_function(t,ak,bk,a0):
     n_coeff = ak.shape[0]
-    constant = a0
     cosines = [cos(2*np.pi*(k+1)*t) for k in range(n_coeff)]
     sines = [sin(2*np.pi*(k+1)*t) for k in range(n_coeff)]
     return np.dot(ak,cosines) + np.dot(bk,sines) + a0
 
-# def A_matrix(x_1s,x_2s,x_1,x_2):
-#     return np.exp(-((x_1s-x_1)**2+(x_2s-x_2)**2))
 
 def box_mask(X, Y, ax, bx, ay, by):
     mask = ((X >= ax) & (X <= bx) & (Y >= ay) & (Y <= by))
     return 1 - mask 
-
-# def box_mask(x, y, ax, bx, ay, by):
-#     """1 outside the box, 0 inside:  [ax,bx] × [ay,by]."""
-#     return ((x < ax) | (x > bx) | (y < ay) | (y > by)).astype(float)
-
 
 def gaussian_2d(x, y, mu_x, mu_y, sigma):
     """Isotropic 2D Gaussian centered at (mu_x, mu_y)."""
@@ -28,53 +20,15 @@ def gaussian_2d(x, y, mu_x, mu_y, sigma):
     exponent = -((x - mu_x)**2 + (y - mu_y)**2) / (2 * sigma**2)
     return coeff * np.exp(exponent)
 
-# def A_obstacle(
-#                mu_x, mu_y, 
-#                x,y,
-#                o1_x, o1_y, 
-#                o2_x, o2_y,
-#                sigma=2,
-#                h=2):
-#     """
-#     Returns obstacle influence:
-#     Sum of Gaussians at two obstacle points,
-#     multiplied by a mask that blocks a rectangular region.
-#     """
-    
-#     # Gaussian contributions from the two obstacles
-#     G1 = gaussian_2d(mu_x, mu_y, o1_x, o1_y,  sigma)
-#     G2 = gaussian_2d( mu_x, mu_y, o2_x, o2_y, sigma)
-#     Gsum = G1 + G2
-    
-#     # Mask box defined by obstacle coordinates (+h padding)
-#     # M = box_mask(x, y,
-#     #              ax=min(o1_x, o2_x),
-#     #              bx=max(o1_x, o2_x),
-#     #              ay=min(o1_y, o2_y),
-#     #              by=max(o1_y, o2_y) + h)
-#     M = box_mask(x, y, o1_x, o2_x, o1_y, o1_y+h)
-    
-#     return Gsum * M
-
 
 def A_obstacle(x_1s,x_2s,x_1,x_2, x_1o1, x_2o1, x_1o2, x_2o2, h=2, sigma=2):
     # dist1 = np.sqrt((x_1s - x_1o1)**2 + (x_2s - x_2o1)**2)
     # dist2 = np.sqrt((x_1s - x_1o2)**2 + (x_2s - x_2o2)**2)
-    # sum_dist = dist1 + dist2
-    # A = gaussian_2d(x_1o1, x_2o1, x_1, x_2, sigma)
-    # B = gaussian_2d(x_1o2, x_2o2, x_1, x_2, sigma)
 
     return (1/((2* np.pi**2 * 0.5**2))*(np.exp(-((x_1o1-x_1)**2+(x_2o1-x_2)**2)) +
     np.exp(-((x_1o2-x_1)**2+(x_2o2-x_2)**2))) *
     box_mask(x_1, x_2, x_1o1, x_1o2, x_2o1, x_2o2+h))
-    # return ((A + B) *
-    # box_mask(x_1, x_2, x_1o1, x_1o2, x_2o1, x_2o2+h))
-
-# def A_source(x_1s,x_2s,x_1,x_2):
-#     # return (scipy.stats.norm.pdf(x_1, loc=x_1s, scale=2) *
-#     #     scipy.stats.norm.pdf(x_2, loc=x_2s, scale=2))
-#     return (1/(np.sqrt(2* np.pi**2 * 2**2)  )) *np.exp(-(1/2**2)*(x_1s-x_1)**2-(1/2**2)*(x_2s-x_2)**2) 
-
+ 
 def A_source(x_1s, x_2s, x_1, x_2, sigma=2):
     """
     2D Gaussian centered at (x_1, x_2), evaluated at (x_1s, x_2s)
